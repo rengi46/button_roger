c1e81fac8d264650b81527fc94bba2b86ef230e4
rogerpuig <rogerpuigdemasa@gmail.com>
2022-09-26 10:28:33 +0200

    service function done

---
+++ services/services.js
@@ -0,0 +1,13 @@
+
+const randomBool = async () => {
+  return new Promise((res,req)=>{
+    setTimeout(
+      ()=>{
+        res(Boolean(Math.round(Math.random())))
+      },
+      1000
+    )
+  })
+}
+
+module.exports = randomBool;
